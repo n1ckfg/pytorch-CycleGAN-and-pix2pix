43bb3839ee95a151789edf68e065c3e59b03da6c
Nick Fox-Gieg <nick@fox-gieg.com>
2023-07-09 14:19:47 -0400

    shell

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-python test.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix
+NAME=facades
+
+python test.py --dataroot ./datasets/$NAME --name pix2pix_$NAME --model pix2pix
 
